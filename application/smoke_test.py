c7377f7d3612d95e6b694397d7696c206a05686b
Boris-86 <brozenman@gmail.com>
2025-04-15 14:38:12 -0600

    quick push

--- application/smoke_test.py
+++ application/smoke_test.py
@@ -3,17 +3,21 @@ import requests
 
 address = "localhost"
 port = "5000"
-SUCCESS = '200'
+SUCCESS = 200
 
-class TestWeatherProject(unittest.TestCase):
+class TestWebApplication(unittest.TestCase):
 
 	def testApp(self):
+		url = f"http://{address}:{port}/"
 		try:
-			http_code = requests.get("${address}:${port}").status_code
-			print("Remote Test: Site is O.K!")
-		except Exception:
-			print("Remote Test: S ite is NOT reachable!")
-		return self.assertTrue(http_code == SUCCESS)
+			response = requests.get(url)
+			http_code = response.status_code
+			print("Remote Test: Web Site is O.K!")
+		except Exception as e:
+            http_code = None
+			print("Remote Test: Web Site ite is NOT reachable!", e)
+		
+		self.assertTrue(http_code == SUCCESS)
 
 if __name__ == '__main__':
     unittest.main()
