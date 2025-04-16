f5ec34fb7943378c3dc39e7233c4d9dfb913475d
Boris-86 <brozenman@gmail.com>
2025-04-16 12:11:50 -0600

    quick push

--- application/smoke_test.py
+++ application/smoke_test.py
@@ -1,15 +1,29 @@
+#===============================================================================
+# Boris Rozenman (c). 
+# 
+# Python Web Application Pipeline
+# Deploy on local environment simulating a whole CI/CD
+#===============================================================================
+
+#===============================================================================
+#                                 Libraries
+#===============================================================================
 import unittest
 import requests
-
+#===============================================================================
+#                           Constants & Variables
+#===============================================================================
 SUCCESS = 200
 adress = "localhost"
 port = "5000"
 
+#===============================================================================
+#                             Classes & Functions
+#===============================================================================
 class TestWebApplication(unittest.TestCase):
     def testApp(self):      
         url = f"http://{adress}:{port}"
-        http_code = None  # Use None to clearly distinguish from 0
-
+        http_code = None  
         try:
             response = requests.get(url)
             http_code = response.status_code
@@ -19,8 +33,12 @@ class TestWebApplication(unittest.TestCase):
             print(f"[X] Smoke Test: Web application at {url} is NOT reachable! [X]")
             print(f"[INFO] Exception: {e}")
             print(f"[INFO] HTTP status code: {http_code}")
-
         self.assertEqual(http_code, SUCCESS)
-
+#===============================================================================
+#                                    MAIN
+#===============================================================================
 if __name__ == '__main__':
     unittest.main()
+#===============================================================================
+#                                 END OF FILE
+#===============================================================================
