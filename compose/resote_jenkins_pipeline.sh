49967bb28b1fc6e6d6f36caa80962f19390e1c64
Boris-86 <brozenman@gmail.com>
2025-04-17 02:49:31 +0300

    compose fix

--- compose/resote_jenkins_pipeline.sh
+++ compose/resote_jenkins_pipeline.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-cd /home/boris/git/stratus/compose
-docker compose -f jenkins-compose.yaml --verbose config
-docker compose -f jenkins-compose.yaml up -d
+docker-compose -f jenkins-compose.yaml --verbose config
+docker-compose -f jenkins-compose.yaml up -d
 sleep 5
 xdg-open http://localhost:8080
